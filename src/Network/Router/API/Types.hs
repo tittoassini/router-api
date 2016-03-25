a5ee4898bd1c67e9a5a5e21fe68c87f4fb5c4886
Pasqualino Titto Assini <tittoassini@gmail.com>
2016-03-25 14:25:28 +0100

    added client side support for patterns

--- src/Network/Router/API/Types.hs
+++ src/Network/Router/API/Types.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor  #-}
+{-# LANGUAGE DeriveGeneric  #-}
 module Network.Router.API.Types(
   module Data.Typed
   ,Config(..)
   ,def
   ,ByType(..),byType
   ,Echo(..)
+  ,ByPattern(..),byPattern
+--  ,module Data.Pattern
   ) where
 
-import Data.Typed
-import Data.Default.Class
+import           Data.Default.Class
+import           Data.Pattern hiding (Con,Var)
+import           Data.Typed
 
 -- |General client configuration
 data Config = Config {ip::String,port::Int,path::String}
@@ -24,8 +29,9 @@ instance Flat Echo
 instance Model Echo
 
 -- |A router indexed by type:
--- Clients can send messages of the given type
--- and receive all messages of the same type sent by other agents
+-- Clients:
+-- send messages of the given type
+-- receive all messages of the same type sent by other agents
 data ByType = ByType AbsType deriving (Eq, Ord, Show, Generic)
 
 instance Flat ByType
@@ -33,4 +39,20 @@ instance Model ByType
 
 -- |Smart constructor
 byType :: Model a => Proxy a -> ByType
-byType proxy = ByType (absType proxy) 
+byType proxy = ByType (absType proxy)
+
+-- |A router index by a pattern of a given type:
+-- Clients:
+-- send messages of the given type
+-- receive all messages of the same type, that matches the given pattern, sent by other agents
+
+data ByPattern = ByPattern AbsType (Pattern WildCard) deriving (Eq, Ord, Show, Generic)
+
+instance Flat ByPattern
+instance Model ByPattern
+
+-- |Smart constructor
+byPattern :: Model a => Proxy a -> Pattern WildCard -> ByPattern
+byPattern proxy = ByPattern (absType proxy)
+
+
